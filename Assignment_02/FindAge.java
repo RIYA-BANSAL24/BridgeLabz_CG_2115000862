be5609f6651cad91f39a7b7b62ef647c42aaa9b8
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-01-22 14:15:25 +0530

    Assignment 02

---
+++ Assignment_02/FindAge.java
@@ -0,0 +1,9 @@
+public class FindAge{
+	public static void main (String args[]){
+		int birth_year = 2000;
+		int cur_year = 2024;
+		int present_age = (cur_year - birth_year) ;
+		System.out.print("Harry's age in 2024 is " + present_age);
+	}
+}
+
