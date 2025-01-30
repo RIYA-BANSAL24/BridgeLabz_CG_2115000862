8feaabaefb5e2b3be93fd71c8089690f014e02be
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-01-30 14:34:46 +0530

    Java Problems Based on Strings

---
+++ Java_String_Problems/RemoveSpecificChar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class RemoveSpecificChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        System.out.print("Enter the character to remove: ");
+        char charToRemove = sc.next().charAt(0);
+        String result = removeCharacter(input, charToRemove);
+        System.out.println("Modified String: " + result);
+    }
+    public static String removeCharacter(String str, char ch) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ch) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+}
